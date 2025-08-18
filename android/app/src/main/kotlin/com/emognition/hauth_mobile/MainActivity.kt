304eb57e0e52a32bb34dc1ce45e5f3e77fdb287d
Kacper Tomczyk <kacperus.tomczyk@gmail.com>
2025-08-18 20:27:42 +0200

    Working QR Scanner

---
+++ android/app/src/main/kotlin/com/emognition/hauth_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.emognition.hauth_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QR Scanner',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<String> scannedCodes = [];
+
+  Future<void> _scanQRCode() async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const QRViewExample()),
+    );
+
+    if (result != null && result is String) {
+      setState(() {
+        scannedCodes.insert(0, result); // latest on top
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR Scanner'),
+      ),
+      body: scannedCodes.isEmpty
+          ? const Center(child: Text("No QR codes scanned yet"))
+          : ListView.builder(
+        itemCount: scannedCodes.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const Icon(Icons.qr_code),
+            title: Text(scannedCodes[index]),
+            subtitle: Text("Scanned #${index + 1}"),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _scanQRCode,
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
+
+class QRViewExample extends StatefulWidget {
+  const QRViewExample({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QRViewExample> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (controller != null) {
+      controller!.pauseCamera();
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: QRView(
+        key: qrKey,
+        onQRViewCreated: _onQRViewCreated,
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      controller.pauseCamera();
+      Navigator.pop(context, scanData.code);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
