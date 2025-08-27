f2537ed2cc1b197312260fabff86b6c5d1dc7695
Karol Zajac <karolzajac.0407@gmail.com>
2025-08-27 20:52:54 +0200

    api gen

--- api/openapi_gen.dart
+++ api/openapi_gen.dart
@@ -1,10 +1,9 @@
-// Openapi Generator last run: : 2025-08-27T20:21:46.769945
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 @Openapi(
-  inputSpec: InputSpec(path: 'lib/api/external.yml'),
+  inputSpec: InputSpec(path: 'api/external.yml'),
   generatorName: Generator.dio,
-  outputDirectory: 'lib/api/generated',
+  outputDirectory: 'api/generated',
   additionalProperties: DioProperties(
     pubName: 'openapi_client',
     pubVersion: '0.1.0',
@@ -13,7 +12,7 @@ import 'package:openapi_generator_annotations/openapi_generator_annotations.dart
 class MyOpenApi {}
 
 /*
-Add-Content lib/api/external.yml "`n# regen $(Get-Date -Format o)"
+Add-Content api/external.yml "`n# regen $(Get-Date -Format o)"
 dart run build_runner clean
 Remove-Item .dart_tool\openapi-generator-cache.json -Force -ErrorAction SilentlyContinue
 dart run build_runner build --delete-conflicting-outputs
