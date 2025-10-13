db8e170858331694071e2de93fd47fb96f4e58c4
Kacper Tomczyk <kacperus.tomczyk@gmail.com>
2025-10-13 17:27:45 +0200

    Create Github Action for automatically building apks on release tags

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,11 +32,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("KEYSTORE_FILE") ?: "shared-key.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs["release"]
         }
     }
 }
