12d3ed88168ad9e4e0996516594453e9e12e3f62
Karol Zając <122168670+poziomk3@users.noreply.github.com>
2025-11-30 14:20:03 +0100

    chore: update contract (#27)
    Co-authored-by: Kacper Tomczyk <37952104+therockey@users.noreply.github.com>

--- lib/providers/api_client_provider.dart
+++ lib/providers/api_client_provider.dart
@@ -1,23 +1,26 @@
 import 'package:dio/dio.dart';
-import 'package:hauth_api_external/hauth_api_external.dart';
+import 'package:heartauth_mobile/heartauth_mobile.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hauth_mobile/providers/server_health_provider.dart';
 import 'package:hauth_mobile/providers/shared_preferences_provider.dart';
 
 class ApiWrapper {
-  final HauthApiExternal _api;
+  final HeartauthMobile _api;
   final Ref ref;
 
   ApiWrapper(this._api, this.ref);
 
-  Future<T> run<T>(Future<T> Function(HauthApiExternal api) call, [bool suppressError = false]) async {
+  Future<T> run<T>(
+    Future<T> Function(HeartauthMobile api) call, [
+    bool suppressError = false,
+  ]) async {
     try {
       final result = await call(_api);
       return result;
     } catch (e) {
       // Network or server error
-      if(!suppressError){
+      if (!suppressError) {
         ref.read(serverHealthProvider.notifier).state =
             ServerHealthStatus.unhealthy;
       }
@@ -34,7 +37,7 @@ final apiUrlProvider = Provider<String>((ref) {
 
 final apiClientProvider = Provider<ApiWrapper>((ref) {
   final apiUrl = ref.watch(apiUrlProvider);
-  final apiClient = HauthApiExternal(
+  final apiClient = HeartauthMobile(
     dio: Dio(BaseOptions(receiveTimeout: Duration(seconds: 20), baseUrl: apiUrl)),
     basePathOverride: apiUrl,
     serializers: standardSerializers,
